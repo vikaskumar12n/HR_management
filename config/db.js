8f1a0389feff4f6beb968e3845914e3fafa97dfe
vikas Bind <147130917+vikaskumar12n@users.noreply.github.com>
2025-03-28 14:26:29 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/hrms" );
+    const conn = await mongoose.connect("MONGO_URI=mongodb+srv://HR_management:pHt4DicC4bMCjKoY@cluster0.mongodb.net/hrms?retryWrites=true&w=majority
+PORT=5010
+" );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
